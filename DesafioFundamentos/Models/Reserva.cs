d3a32a75d6b84023afb94be86f84fea2ea8878ef
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-02-09 12:44:20 -0300

    Projeto de hospedagem em C#

---
+++ DesafioFundamentos/Models/Reserva.cs
@@ -0,0 +1,61 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+             int hospedesTotal = hospedes.Count;
+            if (Suite.Capacidade >= hospedesTotal)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                new Exception("A capacidade da suíte é menor que o número de hóspedes recebidos.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                decimal e = valor * 0.10m;
+                valor = valor - e;
+            }
+
+            return valor;
+        }
+    }
+}
